c68b9bc6d08bf550e97e8c5bf5f6c7fd3119a74c
mephory <mephory@mephory.com>
2015-11-21 08:17:32 +0100

    dont guess the size that needs to be allocated

--- vselect.c
+++ vselect.c
@@ -132,6 +132,60 @@ int getheight(rect_t rect) {
     return abs(rect.end.y - rect.start.y);
 }
 
+char *itoa(int n) {
+    char *str;
+    int size = snprintf(NULL, 0, "%d", n);
+    str = malloc(size);
+    sprintf(str, "%d", n);
+    return str;
+}
+
+char *str_replace(char *orig, char *rep, char *with) {
+    char *result; // the return string
+    char *ins;    // the next insert point
+    char *tmp;    // varies
+    int len_rep;  // length of rep
+    int len_with; // length of with
+    int len_front; // distance between rep and end of last rep
+    int count;    // number of replacements
+
+    if (!orig)
+        return NULL;
+    if (!rep)
+        rep = "";
+    len_rep = strlen(rep);
+    if (!with)
+        with = "";
+    len_with = strlen(with);
+
+    ins = orig;
+    for (count = 0; tmp = strstr(ins, rep); ++count) {
+        ins = tmp + len_rep;
+    }
+
+    // first time through the loop, all the variable are set correctly
+    // from here on,
+    //    tmp points to the end of the result string
+    //    ins points to the next occurrence of rep in orig
+    //    orig points to the remainder of orig after "end of rep"
+    tmp = result = malloc(strlen(orig) + (len_with - len_rep) * count + 1);
+
+    if (!result)
+        return NULL;
+
+    while (count--) {
+        ins = strstr(orig, rep);
+        len_front = ins - orig;
+        tmp = strncpy(tmp, orig, len_front) + len_front;
+        tmp = strcpy(tmp, with) + len_with;
+        orig += len_front + len_rep; // move to next "end of rep"
+    }
+    strcpy(tmp, orig);
+    return result;
+}
+
+
+// cairo stuff
 cairo_status_t read_image(void *closure, unsigned char *data, unsigned int size) {
     FILE *f = (FILE *)closure;
     fread(data, size, 1, f);
@@ -139,8 +193,11 @@ cairo_status_t read_image(void *closure, unsigned char *data, unsigned int size)
 }
 
 FILE *create_png_stream(char *filename) {
-    char command[500];
+    char *command;
+    int size = snprintf(NULL, 0, "convert %s png:- 2>/dev/null", filename);
+    command = malloc(size);
     sprintf(command, "convert %s png:- 2>/dev/null", filename);
+
     FILE *f = popen(command, "r");
     return f;
 }
@@ -234,57 +291,8 @@ point_t to_image_point(int x, int y, state_t state) {
     return p;
 }
 
-char *itoa(int n) {
-    char *str = malloc(50);
-    sprintf(str, "%d", n);
-    return str;
-}
-
-char *str_replace(char *orig, char *rep, char *with) {
-    char *result; // the return string
-    char *ins;    // the next insert point
-    char *tmp;    // varies
-    int len_rep;  // length of rep
-    int len_with; // length of with
-    int len_front; // distance between rep and end of last rep
-    int count;    // number of replacements
-
-    if (!orig)
-        return NULL;
-    if (!rep)
-        rep = "";
-    len_rep = strlen(rep);
-    if (!with)
-        with = "";
-    len_with = strlen(with);
-
-    ins = orig;
-    for (count = 0; tmp = strstr(ins, rep); ++count) {
-        ins = tmp + len_rep;
-    }
-
-    // first time through the loop, all the variable are set correctly
-    // from here on,
-    //    tmp points to the end of the result string
-    //    ins points to the next occurrence of rep in orig
-    //    orig points to the remainder of orig after "end of rep"
-    tmp = result = malloc(strlen(orig) + (len_with - len_rep) * count + 1);
-
-    if (!result)
-        return NULL;
-
-    while (count--) {
-        ins = strstr(orig, rep);
-        len_front = ins - orig;
-        tmp = strncpy(tmp, orig, len_front) + len_front;
-        tmp = strcpy(tmp, with) + len_with;
-        orig += len_front + len_rep; // move to next "end of rep"
-    }
-    strcpy(tmp, orig);
-    return result;
-}
-
 void output(state_t state, options_t options) {
+    // TODO: Don't assume that's enough
     char *output = malloc(5000);
 
     strcpy(output, options.format);
